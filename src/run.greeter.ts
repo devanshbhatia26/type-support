f38ff7611dcda993a7077fffdacdbfe3ba08ab4d
Devansh Bhatia <devanshbhatia@urbanclap.com>
2020-04-03 13:52:11 +0530

    File Creation method added

--- src/run.greeter.ts
+++ src/run.greeter.ts
@@ -1,3 +1,6 @@
-import { Greeter } from './index';
-
-console.log(Greeter("Devansh"));
+import * as fs from 'fs';
+ 
+fs.writeFile('./newFile.txt', "TemporaryFile2", (err) => {
+  if (err) throw err;
+  console.log("File Created Successfully");
+});
