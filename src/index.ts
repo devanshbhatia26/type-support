25484dd5a3bfb1276d94717a83f71314fbef070a
Devansh Bhatia <devanshbhatia@urbanclap.com>
2020-04-03 18:09:30 +0530

    Export Removed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs';
  
-export const Greeter = () => {
-  fs.writeFile('./newFile.txt', "sdgashdgsahsd", (err) => {
+fs.writeFile('./newFile.txt', "sdgashdgsahsd", (err) => {
   if (err) throw err;
   console.log("File Created Successfully");
-})};
+});
