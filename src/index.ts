b3379c2d1859d958290bf0274dd6c612c1bfc356
Devansh Bhatia <devanshbhatia@urbanclap.com>
2020-04-03 18:02:48 +0530

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import * as fs from 'fs';
+ 
+export const Greeter = () => {
+  fs.writeFile('./newFile.txt', "sdgashdgsahsd", (err) => {
+  if (err) throw err;
+  console.log("File Created Successfully");
+})};
--- src/run.greeter.ts
+++
@@ -1,6 +0,0 @@
-import * as fs from 'fs';
- 
-fs.writeFile('./newFile.txt', "TemporaryFile3", (err) => {
-  if (err) throw err;
-  console.log("File Created Successfully");
-});
